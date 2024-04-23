e4125db61865f5c6948260051a4439dd97f5b68b
leizf <Aaz17373510579@163.com>
2024-04-23 20:28:03 +0800

    新增练习-链表递归转成树

--- src/main/java/com/xzit/MyProjectApplication.java
+++ src/main/java/com/xzit/MyProjectApplication.java
@@ -1,9 +1,11 @@
 package com.xzit;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.xzit.practice.tree"})
 public class MyProjectApplication {
 
     public static void main(String[] args) {
