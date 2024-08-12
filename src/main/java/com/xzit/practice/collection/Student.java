bae10c849cad70c06978a556970b21fb6caa1b38
雷泽峰 <leizefeng@formssi.com>
2024-08-12 10:46:43 +0800

    新增：Optional测试

--- src/main/java/com/xzit/practice/collection/Student.java
+++ src/main/java/com/xzit/practice/collection/Student.java
@@ -2,6 +2,7 @@ package com.xzit.practice.collection;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 /**
@@ -15,6 +16,7 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
     private String name;
---
+++ src/test/java/com/xzit/wanger/optional/OptionalTest.java
@@ -0,0 +1,30 @@
+package com.xzit.wanger.optional;
+
+import com.xzit.practice.collection.Student;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+/**
+ * @author leizefeng
+ */
+@SuppressWarnings("ConstantConditions")
+@SpringBootTest
+public class OptionalTest {
+
+    private final Student student = null;
+
+    @Test
+    public void test1() {
+        String name = student == null? null : student.getName();
+        System.out.println(name);
+    }
+
+    @Test
+    public void test2() {
+        String name = Optional.ofNullable(student).map(Student::getName).orElse(null);
+        System.out.println(name);
+    }
+}
